0205940baa370b1b871a8324699899f539c6e7ab
Nishan46 <nipunanishan46@gmail.com>
2022-08-05 18:08:30 +0530

    Finalized

--- Resc/Core.py
+++ Resc/Core.py
@@ -14,21 +14,14 @@ class ENCRYPT:
         self.__symbols_length = (len(self.__symbols) - 1)
 
     def __str__(self):
-        result = f'\n\nHey...\nI am from encrypt class. I think you got what you want now.\nSo... Have a nice day ... !\n\nYour Text = {self.text}\n\nYour Key -->\n{self.__Finaly} '
+        result = f'\n\nHey...\nI am from encrypt Object. I think you got what you want now.\nSo... Have a nice day ... !\n\n'
         return result
 
-    def Get_Code(self,key):
+    def __Get_Code(self,key):
         Codes = (self.__symbols_length - key)
         return Codes
-
-    def Encript(self):       
-        self.__Finaly = ''
-        for steps in track(range((len(self.text))), description="[green bold]Encrypting ..."):
-            self.__index = ENCRYPT.Get_Code(self,self.__symbols.index(self.text[steps]))
-            self.__Finaly += ENCRYPT.Get_Binary(self)                   
-        return self.__Finaly
-
-    def Get_Binary(self):
+    
+    def __Get_Binary(self):
         self.__Binary_String = ""
         for count in range(0,len(self.__Decimal_List)):
             if self.__index >= self.__Decimal_List[count]:
@@ -38,16 +31,80 @@ class ENCRYPT:
                 self.__Binary_String +="0"
         return self.__Binary_String
 
+    def Encript(self):       
+        self.__Finaly = ''
+        for steps in track(range((len(self.text))), description="[green bold] [DONE] --  "):
+            self.__index = ENCRYPT.__Get_Code(self,self.__symbols.index(self.text[steps]))
+            self.__Finaly += ENCRYPT.__Get_Binary(self)
+            sleep(0.01)         
+        return self.__Finaly 
+
+class DECRYPT:
+
+    def __init__(self,key):
+        self.__first_filter = key.translate({ord('│'): None})
+        self.Key = self.__first_filter.translate({ord(' '): None})
+        self.__Peases = []
+        self.__key_length = len(self.Key)
+        self.__front_place = 0
+        self.__Decimal_List = [64,32,16,8,4,2,1]
+        self.__Decrypt_List =[]
+        self.__symbols = Cryptor.Get()
+        self.__symbols_length = (len(self.__symbols) - 1)
+        self.__letter = 0
+        self.__Finaly = ""
+
+    def __str__(self):
+        result = f'\n\nHey...\nI am from decrypt Object. I think you got what you want now.\nSo... Have a nice day ... !\n\n'
+        return result
+
+    def Cutting(self):
+        for NOTHING in range(0,self.__key_length // 7):
+            self.__Peases.append(self.Key[self.__front_place:self.__front_place + 7])
+            self.__front_place += 7
+        DECRYPT.Get_Decimal(self)
+        return self.__Peases
+
+
+    def Get_Decimal(self):
+        for items in self.__Peases:
+            count = 0
+            decimal_int = 0
+            for bits in items:
+                if(bits == "1"):
+                    decimal_int += self.__Decimal_List[count]
+                    count +=1
+                else:
+                    count +=1
+            self.__Decrypt_List.append(decimal_int)
+        return self.__Decrypt_List
+        
+
+    def __Get_Code(self,key):
+        Codes = (self.__symbols_length - key)
+        return Codes        
+
+    def Decrypt(self):
+        DECRYPT.Cutting(self)
+        self.__Finaly = ''
+        for steps in track(range(len(self.__Decrypt_List)-1), description="[green bold] [DONE] --  "):     
+            self.__letter = self.__symbols[DECRYPT.__Get_Code(self,self.__Decrypt_List[steps])]
+            self.__Finaly += self.__letter
+            sleep(0.01)
+        return self.__Finaly
+        
 
 
 
 
 
 
-# def Decrypt(Texts):
-#     key = ''
-#     for items in Texts:
-#         Code = symbols[DecryptCode(symbols.index(items),symbols_length)]
-#         sleep(0.1)
-#         key += Code
-#     return key
+# def Decrypt(self):
+#         DECRYPT.Cutting(self)
+#         sleep(1)
+#         DECRYPT.Get_Decimal(self)
+#         sleep(1)
+#         for Decimals in self.__Decrypt_List:
+#             self.__letter = self.__symbols[DECRYPT.__Get_Code(self,Decimals)]
+#             self.__Finaly += self.__letter
+#         return self.__Finaly
--- main.py
+++ main.py
@@ -1,29 +1,42 @@
+from distutils import core
 from time import sleep
 from Resc import Core as Core , styles
 from Resc.styles import St as styles
 from rich import print
 
-# try:
-    
-# except:
-#     if KeyboardInterrupt:
-#         print("[red bold][Program] [Exited] !!!![/red bold]")
+try:
+
+    styles.Consols("\n[STARTING]\n",colours="blue")
+    sleep(1)
+
+    styles.Pointpans()
+    que = styles.InputWith_Choice("[yellow]Choose your [green]choice",["1","2"],"1")
+
+    if(que == "1"):
+        name = str(styles.Input("Enter Text there "))
+        EnCriptor = Core.ENCRYPT(name)
+        styles.pansInfo("blue bold",EnCriptor)
+        styles.pans("purple bold","Key Genarated ! ","green bold",EnCriptor.Encript())
+        
+    else:
+        name = str(styles.Input("Enter Key there "))
+        Decryptor = Core.DECRYPT(name)
+        styles.pansInfo("blue bold",Decryptor)
+        styles.pans("purple bold","Key Restored ! ","green bold",Decryptor.Decrypt())
 
 
-styles.Consols("\n[STARTING]\n",colours="blue")
-sleep(1)
+except KeyboardInterrupt:
+    print("[red bold][Program] [Exited][/red bold]")
+        
 
-styles.Pointpans()
-que = styles.InputWith_Choice("[yellow]Choose your [green]choice",["1","2"],"1")
+except ValueError as e:
+    print(f"\n[red bold]Yor Text is Not Supported !!!![/red bold]\n\n")
+    styles.pansInfo("yellow bold",f"[ERROR TYPE]\n\n{e}")
 
-if(que == "1"):
-    name = str(styles.Input("Enter Text there "))
-    EnCriptor = Core.ENCRYPT(name)
-    styles.pans("purple bold","Key Genarated ! ","green bold",EnCriptor.Encript())
-    styles.pansInfo("blue bold",EnCriptor)
-else:
-    name = str(styles.Input("Enter Key there "))
-    styles.pans("green",f"Here we are ...[/green]\n [red]---- Key ----\n{name}[/red]\n [yellow]--- Clear Text ---[/yellow]","green",Core.Decrypt(name))
+except Exception as e:
+    print(f"[red bold][UNHANDLED ERROR]\n\nERROR = {e}")
+
+
 
 
 
